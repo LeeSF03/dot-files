91caa05e8aaaaf86f2e427beca619149df82033a
LeeSF03 <shuenfeilee@gmail.com>
2024-10-29 20:20:15 +0800

    chore: modfied wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -283,73 +283,82 @@ config.leader = {
 --   { key = "L", mods = "LEADER", action = wezterm.action({ EmitEvent = "load_state" }) },
 -- }
 config.keys = { -- This will create a new split and run the `top` program inside it
-{
-    key = "=",
-    mods = "LEADER",
-    action = act.SplitPane {
-        direction = "Right",
-        -- command = { args = { "top" } },
-        size = {
-            Percent = 50
+    {
+        key = "=",
+        mods = "LEADER",
+        action = act.SplitPane {
+            direction = "Right",
+            -- command = { args = { "top" } },
+            size = {
+                Percent = 50
+            }
         }
-    }
-}, {
-    key = "-",
-    mods = "LEADER",
-    action = act.SplitPane {
-        direction = "Down",
-        -- command = { args = { "top" } },
-        size = {
-            Percent = 50
+    }, {
+        key = "-",
+        mods = "LEADER",
+        action = act.SplitPane {
+            direction = "Down",
+            -- command = { args = { "top" } },
+            size = {
+                Percent = 50
+            }
         }
+    }, {
+        key = "h",
+        mods = "LEADER",
+        action = act.ActivatePaneDirection("Left")
+    }, {
+        key = "j",
+        mods = "LEADER",
+        action = act.ActivatePaneDirection("Down")
+    }, {
+        key = "k",
+        mods = "LEADER",
+        action = act.ActivatePaneDirection("Up")
+    }, {
+        key = "l",
+        mods = "LEADER",
+        action = act.ActivatePaneDirection("Right")
+    }, {
+        key = "x",
+        mods = "LEADER",
+        action = act.CloseCurrentPane {
+            confirm = true
+        }
+    }, {
+        key = 'h',
+        mods = 'CTRL|ALT',
+        action = act.AdjustPaneSize {'Left', 1}
+    }, {
+        key = 'j',
+        mods = 'CTRL|ALT',
+        action = act.AdjustPaneSize {'Down', 1}
+    }, {
+        key = 'k',
+        mods = 'CTRL|ALT',
+        action = act.AdjustPaneSize {'Up', 1}
+    }, {
+        key = 'l',
+        mods = 'CTRL|ALT',
+        action = act.AdjustPaneSize {'Right', 1}
+    }, {
+        key = 'k',
+        mods = 'CTRL|SHIFT',
+        action = act.ScrollByPage(-0.5)
+    }, {
+        key = 'j',
+        mods = 'CTRL|SHIFT',
+        action = act.ScrollByPage(0.5)
+    }, {
+        key = 'n',
+        mods = 'CTRL|SHIFT',
+        action = act.SpawnWindow
+    }, {
+        key = 'h',
+        mods = 'CTRL|SHIFT',
+        action = act.Hide
     }
-}, {
-    key = "h",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Left")
-}, {
-    key = "j",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Down")
-}, {
-    key = "k",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Up")
-}, {
-    key = "l",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Right")
-}, {
-    key = "x",
-    mods = "LEADER",
-    action = act.CloseCurrentPane {
-        confirm = true
-    }
-}, {
-    key = 'h',
-    mods = 'CTRL|ALT',
-    action = act.AdjustPaneSize {'Left', 1}
-}, {
-    key = 'j',
-    mods = 'CTRL|ALT',
-    action = act.AdjustPaneSize {'Down', 1}
-}, {
-    key = 'k',
-    mods = 'CTRL|ALT',
-    action = act.AdjustPaneSize {'Up', 1}
-}, {
-    key = 'l',
-    mods = 'CTRL|ALT',
-    action = act.AdjustPaneSize {'Right', 1}
-}, {
-    key = 'p',
-    mods = 'CTRL|SHIFT',
-    action = act.ScrollByPage(-0.5)
-}, {
-    key = 'n',
-    mods = 'CTRL|SHIFT',
-    action = act.ScrollByPage(0.5)
-}}
+}
 
 config.key_tables = {
     search_mode = {{
