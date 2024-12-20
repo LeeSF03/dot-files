0463480e6844d59ae857c6c77baa0f2fc62548a5
LeeSF03 <shuenfeilee@gmail.com>
2024-12-21 01:38:04 +0800

    fix: change eslint to eslint_d in nvim-lint config

--- nvim/lua/config/lint.lua
+++ nvim/lua/config/lint.lua
@@ -2,10 +2,10 @@ local lint = require("lint")
 
 -- Available Linters: https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#available-linters
 lint.linters_by_ft = {
-  typescript = { "eslint" },
-  javascript = { "eslint" },
-  typescriptreact = { "eslint" },
-  javascriptreact = { "eslint" },
+  typescript = { "eslint_d" },
+  javascript = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
 }
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
