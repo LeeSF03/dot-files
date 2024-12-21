9a44c6c5287626709b7db75b4a37102b5d01af64
LeeSF03 <shuenfeilee@gmail.com>
2024-12-21 23:40:47 +0800

    chore: add events to lint and conform

--- nvim/lua/config/conform.lua
+++ nvim/lua/config/conform.lua
@@ -1,5 +1,15 @@
-require("conform").setup({
+local conform = require("conform")
+
+conform.setup({
+  -- Available formatters: https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
   formatters_by_ft = {
     javascript = { "prettierd", "prettier", stop_after_first = true },
   },
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
 })
