c43c2fde2663dda18bea0ac63722b792571fca3c
LeeSF03 <shuenfeilee@gmail.com>
2024-12-21 23:41:04 +0800

    feat: update cmdline popup highlight

--- nvim/lua/config/wilder.lua
+++ nvim/lua/config/wilder.lua
@@ -4,10 +4,13 @@ wilder.setup({ modes = { ':', '/', '?' } })
 wilder.set_option('renderer', wilder.popupmenu_renderer(
   wilder.popupmenu_border_theme({
     highlights = {
-      border = wilder.make_hl('Border', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#89b4fa' } }),
-      accent = wilder.make_hl('Accent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#ea76cb' } }),
-      selected = wilder.make_hl('SelectedCommand', 'Pmenu',
-        { { a = 1 }, { a = 1 }, { foreground = '#ea76cb', background = '#45475a' } }),
+      default = wilder.make_hl('WilderDefault', 'Pmenu', { { a = 1 }, { a = 1 }, { background = "#181825" } }),
+      border = wilder.make_hl('WilderBorder', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#89b4fa', background = "#181825" } }),
+      accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#ea76cb', background = "#181825" } }),
+      selected = wilder.make_hl('WilderSelected', 'Pmenu',
+        { { a = 1 }, { a = 1 }, { background = '#2b2b3c', foreground = '#ea76cb' } }),
+      selected_accent = wilder.make_hl('WilderSelectedAccent', 'Pmenu',
+        { { a = 1 }, { a = 1 }, { background = '#2b2b3c', foreground = '#ea76cb', underline = true } }),
     },
     -- 'single', 'double', 'rounded' or 'solid'
     -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
