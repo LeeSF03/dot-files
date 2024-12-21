74cb0567e2af26367fea6b1829566427c4973537
LeeSF03 <shuenfeilee@gmail.com>
2024-12-21 23:39:07 +0800

    chore: remove blink config from lsp plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,33 +11,6 @@ return {
 
     -- use a release tag to download pre-built binaries
     version = 'v0.*',
-    opts = {
-      keymap = {
-          ['<C-s>'] = { 'show', 'show_documentation', 'hide_documentation' },
-          ['<C-e>'] = { 'hide' },
-          ['<C-y>'] = { 'select_and_accept' },
-
-          ['<C-k>'] = { 'select_prev', 'fallback' },
-          ['<C-j>'] = { 'select_next', 'fallback' },
-
-          ['<C-h>'] = { 'scroll_documentation_up', 'fallback' },
-          ['<C-l>'] = { 'scroll_documentation_down', 'fallback' },
-      },
-      documentation = {
-        auto_show = true,
-      },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono'
-      },
-      completion = {
-        menu = {
-          border = 'rounded',
-        },
-      },
-      -- experimental signature help support
-      signature = { enabled = true }
-    },
     opts_extend = { "sources.default" }
   },
   {
