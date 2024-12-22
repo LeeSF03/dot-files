5512c36fc02d19a63e377d0f08cd79d812acbd06
LeeSF03 <shuenfeilee@gmail.com>
2024-12-22 19:00:22 +0800

    feat: add key map to create unname buffer

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -4,4 +4,5 @@ bufferline.setup()
 
 vim.keymap.set('n', '<leader>]', '<cmd>BufferLineCycleNext<CR>', { desc = 'Switch to next buffer' })
 vim.keymap.set('n', '<leader>[', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Switch to previous buffer' })
-vim.keymap.set('n', '<leader>bd', ':bdelete<cr>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', '<leader>bn', '<cmd>:enew<CR>', { desc = 'Create new unname buffer' })
