253328a46d9c1ae69b6c609545710a832592a458
LeeSF03 <shuenfeilee@gmail.com>
2024-12-23 21:38:29 +0800

    feat: add gitsign keymap

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -1,3 +1,23 @@
 local gitsigns = require('gitsigns')
 
-gitsigns.setup()
+gitsigns.setup({
+  current_line_blame = true,
+    signs = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+})
+
+vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>', { desc = 'Git Preview Hunk' })
