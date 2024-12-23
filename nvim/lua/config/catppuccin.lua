50bc2714f95c9b4d007af7b2642cf2e660aba910
LeeSF03 <shuenfeilee@gmail.com>
2024-12-23 21:36:48 +0800

    chore: update catppuccin config

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -1,1 +1,8 @@
+local catppuccin = require("catppuccin")
+
+catppuccin.setup({
+  transparent_background = true,
+  integrations = { blink_cmp = true },
+})
+
 vim.cmd.colorscheme "catppuccin-mocha"
