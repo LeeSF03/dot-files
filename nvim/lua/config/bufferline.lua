ff90fafc674c38d7b26deb3cd830c1b04014e553
LeeSF03 <shuenfeilee@gmail.com>
2024-12-24 14:21:20 +0800

    feat: add swap buffer keymap

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -4,5 +4,7 @@ bufferline.setup()
 
 vim.keymap.set('n', '<leader>]', '<cmd>BufferLineCycleNext<CR>', { desc = 'Switch to next buffer' })
 vim.keymap.set('n', '<leader>[', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Switch to previous buffer' })
+vim.keymap.set('n', '<leader>}', '<cmd>BufferLineMoveNext<CR>', { desc = 'Swap with next buffer' })
+vim.keymap.set('n', '<leader>{', '<cmd>BufferLineMovePrev<CR>', { desc = 'Swap with previous buffer' })
 vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = 'Delete current buffer' })
 vim.keymap.set('n', '<leader>bn', '<cmd>:enew<CR>', { desc = 'Create new unname buffer' })
