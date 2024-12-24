5837e378b96c204dc4e2d63e93fda193cfd4e6cb
LeeSF03 <shuenfeilee@gmail.com>
2024-12-24 20:42:49 +0800

    feat: update toggleterm keymap

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -16,11 +16,11 @@ toggleterm.setup({
 	}
 })
 
-vim.keymap.set('n', '<C-t><C-t>', function() vim.cmd(tostring(vim.v.count1) .. "ToggleTerm") end, { desc = 'Open terminal in floating' })
-vim.keymap.set('n', '<C-t><C-v>', function() vim.cmd(tostring(vim.v.count1) .. "ToggleTerm direction=vertical") end, { desc = 'Open terminal in vertical split' })
-vim.keymap.set('n', '<C-t><C-s>', function() vim.cmd(tostring(vim.v.count1) .. "ToggleTerm direction=horizontal") end, { desc = 'Open terminal in horizontal split' })
-vim.keymap.set('t', '<C-t><C-n>', '<C-\\><C-n>', { desc = 'Turn into normal mode' })
-vim.keymap.set('n', '<C-t><C-a>', '<cmd>ToggleTermToggleAll<CR>', { desc = 'Toggle all terminal' })
+vim.keymap.set('n', '<C-t>t', function() vim.cmd(tostring(vim.v.count1) .. "ToggleTerm") end, { desc = 'Open terminal in floating' })
+vim.keymap.set('n', '<C-t>v', function() vim.cmd(tostring(vim.v.count1) .. "ToggleTerm direction=vertical") end, { desc = 'Open terminal in vertical split' })
+vim.keymap.set('n', '<C-t>s', function() vim.cmd(tostring(vim.v.count1) .. "ToggleTerm direction=horizontal") end, { desc = 'Open terminal in horizontal split' })
+vim.keymap.set('t', '<C-t>n', '<C-\\><C-n>', { desc = 'Turn into normal mode' })
+vim.keymap.set('n', '<C-t>a', '<cmd>ToggleTermToggleAll<CR>', { desc = 'Toggle all terminal' })
 vim.keymap.set('n', '<leader>ft', '<cmd>TermSelect<CR>', { desc = 'Find terminal' })
 -- vim.keymap.set({ 'n', 'i' }, '<C-t><C-t>', '<cmd>ToggleTerm<CR>', { desc = 'Open terminal in vertical split' })
 -- vim.keymap.set('n', '<C-t><C-v>', '<cmd>ToggleTerm direction=vertical<CR>', { desc = 'Open terminal in vertical split' })
