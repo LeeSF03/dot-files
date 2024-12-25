8e66709d7c48a2ed02fb8dd6205bb89b5e8be5a1
LeeSF03 <shuenfeilee@gmail.com>
2024-12-25 12:19:35 +0800

    chore: update oil.nvim config

--- nvim/lua/config/oil.lua
+++ nvim/lua/config/oil.lua
@@ -4,6 +4,8 @@ local HEIGHT_RATIO = 0.8 -- You can change this
 local WIDTH_RATIO = 0.5  -- You can change this too
 
 oil.setup({
+  default_file_explorer = true,
+  delete_to_trash = true,
   view_options = {
     show_hidden = true,
   },
