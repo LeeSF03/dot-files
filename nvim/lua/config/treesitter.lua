1a18af5045b15368438fec5424bda75ec36ce9c0
LeeSF03 <shuenfeilee@gmail.com>
2024-12-27 02:38:15 +0800

    chore: added autopair and autotag

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -6,7 +6,7 @@ require('nvim-treesitter.install').compilers = {"zig", "clang", "gcc"}
 
 ---@diagnostic disable-next-line: missing-fields
 configs.setup({
-  ensure_installed = { "c", "lua", "javascript", "html", "markdown", "markdown_inline", "cpp" },
+  ensure_installed = { "c", "lua", "javascript", "html", "markdown", "markdown_inline", "cpp", "typescript", "tsx" },
   highlight = { enable = true },
   indent = { enable = true },
 })
