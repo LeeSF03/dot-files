87042090bf93b8428c341b42f68846649547cd5f
LeeSF03 <shuenfeilee@gmail.com>
2024-12-29 00:25:45 +0800

    feat: add custom icons to docker compose files

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -1,6 +1,22 @@
 local bufferline = require('bufferline')
+local devicons = require('nvim-web-devicons')
 
-bufferline.setup()
+bufferline.setup({
+  options = {
+    get_element_icon = function(element)
+      -- element consists of {filetype: string, path: string, extension: string, directory: string}
+      -- This can be used to change how bufferline fetches the icon
+      -- for an element e.g. a buffer or a tab.
+      -- e.g.
+      if (string.find(element.path, "docker")) then
+        local icon, hl = devicons.get_icon_by_filetype("dockerfile", { default = false })
+        return icon, hl
+      end
+      local icon, hl = devicons.get_icon_by_filetype(element.filetype, { default = false })
+      return icon, hl
+    end,
+  }
+})
 
 vim.keymap.set('n', '<leader>]', '<cmd>BufferLineCycleNext<CR>', { desc = 'Switch to next buffer' })
 vim.keymap.set('n', '<leader>[', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Switch to previous buffer' })
