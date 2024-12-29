2f9675e4537e15a833c76d1bf3d6cf0754e3af0c
LeeSF03 <shuenfeilee@gmail.com>
2024-12-29 19:22:26 +0800

    chore: change lualine and bufferline style

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -2,7 +2,44 @@ local bufferline = require('bufferline')
 local devicons = require('nvim-web-devicons')
 
 bufferline.setup({
+  highlights = {
+    fill = {
+      bg = '#181825',
+    },
+    background = {
+      bg = '#232634',
+    },
+    tab = {
+      fg = '#00ff00',
+      bg = '#00ff00',
+    },
+    buffer_visible = {
+      bg = '#232634',
+    },
+    close_button = {
+      bg = '#232634',
+    },
+    close_button_visible = {
+      bg = '#232634',
+    },
+    separator = {
+      -- fg = '#11111b',
+      fg = '#181825',
+      bg = '#232634',
+    },
+    separator_visible = {
+      fg = '#181825',
+      bg = '#232634',
+    },
+    separator_selected = {
+      fg = '#181825',
+    },
+  },
   options = {
+    separator_style = "slant",
+    -- indicator = {
+    --   style = "none",
+    -- },
     get_element_icon = function(element)
       -- element consists of {filetype: string, path: string, extension: string, directory: string}
       -- This can be used to change how bufferline fetches the icon
