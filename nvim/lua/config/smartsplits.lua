c6815fddb3f2e3a640335cf79d8dce2aeb18046b
LeeSF03 <shuenfeilee@gmail.com>
2024-12-29 23:09:54 +0800

    chore: update switching window/pane key map

--- nvim/lua/config/smartsplits.lua
+++ nvim/lua/config/smartsplits.lua
@@ -8,7 +8,8 @@ vim.keymap.set('n', '<A-j>', function() smartsplits.resize_down(1) end)
 vim.keymap.set('n', '<A-k>', function() smartsplits.resize_up(1) end)
 vim.keymap.set('n', '<A-l>', function() smartsplits.resize_right(1) end)
 -- moving between splits
-vim.keymap.set('n', '<C-h>', smartsplits.move_cursor_left)
-vim.keymap.set('n', '<C-j>', smartsplits.move_cursor_down)
-vim.keymap.set('n', '<C-k>', smartsplits.move_cursor_up)
-vim.keymap.set('n', '<C-l>', smartsplits.move_cursor_right)
+-- the letters are capital even with shift because of wezterm
+vim.keymap.set('n', '<C-S-H>', smartsplits.move_cursor_left)
+vim.keymap.set('n', '<C-S-J>', smartsplits.move_cursor_down)
+vim.keymap.set('n', '<C-S-K>', smartsplits.move_cursor_up)
+vim.keymap.set('n', '<C-S-L>', smartsplits.move_cursor_right)
