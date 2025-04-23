65b60e4c885b401e33c2d4638ca9bdd64ca3c655
LeeSF03 <shuenfeilee@gmail.com>
2025-04-23 12:06:11 +0800

    feat: remove highlight when esc is pressed

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,6 +15,9 @@ vim.keymap.set("n", "<leader>-", "<CMD>Oil --float<CR>", { desc = "Open parent d
 vim.keymap.set({"n", "i", "v" }, "<C-u>", "<C-u>zz", { desc = "Scroll up half page" })
 vim.keymap.set({"n", "i", "v" }, "<C-d>", "<C-d>zz", { desc = "Scroll down half page" })
 
+-- esc remap to disable highlight
+vim.keymap.set({ "n" }, "<Esc>", "<Esc> <CMD>noh<CR>", { desc = "Disable highlight" })
+
 -- disable keymaps
 vim.keymap.set("n", ";", "", { desc = "Disable ; keymap" })
 
