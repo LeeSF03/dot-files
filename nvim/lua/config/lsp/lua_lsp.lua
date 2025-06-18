80630f41c887e5b1b0d7b4042e3437f895e7e912
LeeSF03 <shuenfeilee@gmail.com>
2025-06-18 16:14:47 +0800

    chore: lazy commit

--- nvim/lua/config/lsp/lua_lsp.lua
+++ nvim/lua/config/lsp/lua_lsp.lua
@@ -3,37 +3,38 @@
 -- local capabilities = blink.get_lsp_capabilities()
 
 vim.lsp.config.lua_ls = {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
-  settings = {
-    Lua = {
-      runtime = {
-        -- Specify LuaJIT for Neovim
-        version = "LuaJIT",
-        -- Include Neovim runtime files
-        path = vim.split(package.path, ";"),
-      },
-      diagnostics = {
-        -- Recognize `vim` as a global
-        globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-    hint = {
-      enable = true,
-      arrayIndex = "Enable",
-      await = true,
-      paramName = "All",
-      paramType = true,
-      semicolon = "Disable",
-      setType = true,
-    },
-    telemetry = {
-      enable = false,
-    },
-  },
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Specify LuaJIT for Neovim
+				version = "LuaJIT",
+				-- Include Neovim runtime files
+				path = vim.split(package.path, ";"),
+			},
+			diagnostics = {
+				-- Recognize `vim` as a global
+				globals = { "vim" },
+				disable = { "trailing-space" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+		hint = {
+			enable = true,
+			arrayIndex = "Enable",
+			await = true,
+			paramName = "All",
+			paramType = true,
+			semicolon = "Disable",
+			setType = true,
+		},
+		telemetry = {
+			enable = false,
+		},
+	},
 }
