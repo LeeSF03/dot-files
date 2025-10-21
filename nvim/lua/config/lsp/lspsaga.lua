7bfeb73896563d130ba4cba6bc3ede346378b5bf
LeeSF03 <shuenfeilee@gmail.com>
2025-10-21 18:58:44 +0800

    chore: lazy commit

--- nvim/lua/config/lsp/lspsaga.lua
+++ nvim/lua/config/lsp/lspsaga.lua
@@ -11,11 +11,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 					exec = "<CR>",
 				},
 			},
-			-- rename = {
-			--   keys = {
-			--     quit = 'q',
-			--   }
-			-- },
 			definition = {
 				keys = {
 					quit = "q",
@@ -23,63 +18,34 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			},
 		})
 
-		-- Disabled because using blink.cmp
-		-- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-		-- hover
-		-- vim.keymap.set('n', 'K', '<CMD>Lspsaga hover_doc<CR>', { desc = "Hover action from LSP", buffer = ev.buf })
-		-- vim.keymap.set('n', 'K', '<CMD>Lspsaga hover_doc ++<CR>', { desc = "Enter hover", buffer = ev.buf })
-
 		-- diagnostics
-		vim.keymap.set(
-			"n",
-			"gsc",
-			"<CMD>Lspsaga show_cursor_diagnostics<CR>",
-			{ desc = "Show cursor diagnostics", buffer = ev.buf }
-		)
-		vim.keymap.set(
-			"n",
-			"gss",
-			"<CMD>Lspsaga show_line_diagnostics<CR>",
-			{ desc = "Show line diagnostics", buffer = ev.buf }
-		)
-		vim.keymap.set(
-			"n",
-			"gsb",
-			"<CMD>Lspsaga show_buf_diagnostics<CR>",
-			{ desc = "Show buffer diagnostics", buffer = ev.buf }
-		)
-		vim.keymap.set(
-			"n",
-			"gsw",
-			"<CMD>Lspsaga show_workspace_diagnostics<CR>",
-			{ desc = "Show workplace diagnostics", buffer = ev.buf }
-		)
-		vim.keymap.set(
-			"n",
-			"gsp",
-			"<CMD>Lspsaga diagnostic_jump_prev<CR>",
-			{ desc = "Jump to previous diagnostic", buffer = ev.buf }
-		)
-		vim.keymap.set(
-			"n",
-			"gsn",
-			"<CMD>Lspsaga diagnostic_jump_next<CR>",
-			{ desc = "Jump to next diagnostic", buffer = ev.buf }
-		)
+    -- stylua: ignore start
+		vim.keymap.set("n", "gsc", "<CMD>Lspsaga show_cursor_diagnostics<CR>", { desc = "Show cursor diagnostics", buffer = ev.buf })
+		vim.keymap.set("n", "gss", "<CMD>Lspsaga show_line_diagnostics<CR>", { desc = "Show line diagnostics", buffer = ev.buf })
+		vim.keymap.set("n", "gsb", "<CMD>Lspsaga show_buf_diagnostics<CR>", { desc = "Show buffer diagnostics", buffer = ev.buf })
+		vim.keymap.set("n", "gsw", "<CMD>Lspsaga show_workspace_diagnostics<CR>", { desc = "Show workplace diagnostics", buffer = ev.buf })
+		vim.keymap.set("n", "gsp", "<CMD>Lspsaga diagnostic_jump_prev<CR>", { desc = "Jump to previous diagnostic", buffer = ev.buf })
+		vim.keymap.set("n", "gsn", "<CMD>Lspsaga diagnostic_jump_next<CR>", { desc = "Jump to next diagnostic", buffer = ev.buf })
+
+		-- disable default lsp keymaps
+		pcall(vim.keymap.del, "n", "grn")
+		pcall(vim.keymap.del, "n", "gra")
+		pcall(vim.keymap.del, "n", "grr")
+		pcall(vim.keymap.del, "n", "gri")
 
 		-- definition
-		vim.keymap.set("n", "gdp", "<CMD>Lspsaga peek_definition<CR>", { desc = "Peek defenition", buffer = ev.buf })
-		vim.keymap.set("n", "gdo", "<CMD>Lspsaga goto_definition<CR>", { desc = "Go to defenition", buffer = ev.buf })
+		vim.keymap.set("n", "gp", "<CMD>Lspsaga peek_definition<CR>", { desc = "Peek defenition", buffer = ev.buf })
+		vim.keymap.set("n", "gd", "<CMD>Lspsaga goto_definition<CR>", { desc = "Go to defenition", buffer = ev.buf })
 
 		-- others
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration", buffer = ev.buf })
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation", buffer = ev.buf })
-		vim.keymap.set("n", "gr", telescope_builtin.lsp_references, { desc = "Go to references", buffer = ev.buf })
+		vim.keymap.set( "n", "<leader>gr", telescope_builtin.lsp_references, { desc = "Go to references", buffer = ev.buf })
 
 		vim.keymap.set("n", "<leader>rn", "<CMD>Lspsaga rename<CR>", { desc = "Rename variable", buffer = ev.buf })
-		-- vim.keymap.set({ 'n', 'v' }, '<leader>ca', '<CMD>Lspsaga code_action<CR>', { desc = "Code Actions", buffer = ev.buf })
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions", buffer = ev.buf })
+		-- stylua: ignore end
+
 		vim.keymap.set("n", "<leader>cf", function()
 			vim.lsp.buf.format({ async = true })
 		end, { desc = "Code Format", buffer = ev.buf })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,11 +8,6 @@ return {
 			"nvim-telescope/telescope-ui-select.nvim",
 		},
 		config = function()
-			vim.api.nvim_set_hl(0, "FloatTitle", { bg = "#1e1e2e", fg = "#89b4fa"  })
-			vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg = "#1e1e2e" })
-			vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "#1e1e2e" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e" })
-			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1e1e2e", fg = "#89b4fa" })
 			require("config.telescope")
 		end,
 		-- event = 'VeryLazy',
