4fad9d9ffc9e8a43f313df3bbd8fd9fc19d71ab2
LeeSF03 <shuenfeilee@gmail.com>
2025-10-25 18:02:53 +0800

    feat(nivm): change conflict colors

--- nvim/lua/plugins/conflict.lua
+++ nvim/lua/plugins/conflict.lua
@@ -10,14 +10,14 @@ local colors = {
 	overlay0 = "#6c7086",
 }
 
--- Labels (conflict markers: <<<<<<<, =======, >>>>>>>)
+-- Labels (conflict markers: <<<<<<<, |||||||, >>>>>>>)
 vim.api.nvim_set_hl(0, "GitConflictCurrentLabel", { fg = colors.base, bg = colors.red, bold = true })
 vim.api.nvim_set_hl(0, "GitConflictIncomingLabel", { fg = colors.base, bg = colors.green, bold = true })
 vim.api.nvim_set_hl(0, "GitConflictAncestorLabel", { fg = colors.text, bg = colors.surface1, bold = true })
 
 -- Content blocks (solid background colors)
-vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#7f3b4c", fg = colors.text }) -- darker red variant
-vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#3b7f57", fg = colors.text }) -- darker green variant
+vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e1e21", fg = colors.text }) -- darker red variant
+vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#22322b", fg = colors.text }) -- darker green variant
 vim.api.nvim_set_hl(0, "GitConflictAncestor", { bg = colors.surface1, fg = colors.text }) -- close to background
 vim.api.nvim_set_hl(0, "GitConflictBase", { bg = colors.surface0, fg = colors.text }) -- neutral gray
 
