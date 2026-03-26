4cc20df6a56d76547b922b4df06eb8fe1309b35a
LeeSF03 <shuenfeilee@gmail.com>
2026-03-26 20:53:22 +0800

    feat: add gitsign preview hunk inline keymap

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,12 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    transparent_background = true,
-    integrations = { blink_cmp = true },
-  },
-  init = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		transparent_background = true,
+		integrations = { blink_cmp = true, treesitter = true, diffview = true, fidget = true },
+	},
+	init = function()
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,26 +1,29 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			current_line_blame = true,
-			-- numhl = true,
-			signs = {
-				add = { text = "┃" },
-				change = { text = "┃" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
-			},
-			signs_staged = {
-				add = { text = "┃" },
-				change = { text = "┃" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
-			},
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "Gitsigns" },
+	-- stylua: ignore start
+	keys = {
+    { "+", mode = { "n" },function() require("gitsigns").preview_hunk_inline() end, desc = "Preview diff hunk inline" }
+  },
+	-- stylua: ignore end
+	opts = {
+		current_line_blame = true,
+		signs = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signs_staged = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
 		},
 	},
 }
