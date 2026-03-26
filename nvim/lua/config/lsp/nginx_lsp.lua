8c04e389471de58e6ae5549e88fe03c2488d0eb6
LeeSF03 <shuenfeilee@gmail.com>
2026-03-26 15:11:41 +0800

    feat: add icons to nvim-devicons config

--- nvim/lua/config/lsp/nginx_lsp.lua
+++ nvim/lua/config/lsp/nginx_lsp.lua
@@ -1,8 +1,12 @@
-local lspconfig = require("lspconfig")
-local blink = require("blink.cmp")
-local capabilities = blink.get_lsp_capabilities()
-
-lspconfig.nginx_language_server.setup({
-  capabilities = capabilities,
+vim.filetype.add({
+	extension = { conf = "nginx" },
+	filename = { ["nginx.conf"] = "nginx" },
+	pattern = { [".*/nginx/.*%.conf"] = "nginx" },
 })
 
+vim.lsp.config("nginx-language-server", {
+	cmd = { "nginx-language-server" },
+	filetypes = { "nginx" },
+	root_markers = { "nginx.conf", ".git" },
+})
+vim.lsp.enable("nginx-language-server")
